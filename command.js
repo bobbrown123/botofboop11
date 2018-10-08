f8edeb7e5e9e25d35aef6377136ebfc474efff48
bobbrown123 <43970750+bobbrown123@users.noreply.github.com>
2018-10-08 15:55:04 -0400

    Create command.js

---
+++ command.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+module.exports = (client, member, message) => {
+  const byeChannel = member.guild.channels.find('name', 'welcome');
+  if (byeChannel) {
+    let byeEmbed = new Discord.RichEmbed()
+    .setTitle("Member has left!")
+    .setThumbnail(member.user.displayAvatarURL)
+    .setDescription(`Bye ${member.user}, Seeya next time!`)
+    .setColor("#4286f4")
+    .setFooter(`We now have ${member.guild.memberCount} members!`)
+    .setTimestamp();
+    byeChannel.send(byeEmbed)
+  }
+}
